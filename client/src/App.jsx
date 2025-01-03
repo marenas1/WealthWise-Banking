3e475c625b0c48553424dc97a78a28e62671d127
marenas1 <140763896+marenas1@users.noreply.github.com>
2025-01-03 10:04:32 -0500

    initial commit, plaid connected on backend

---
+++ client/src/App.jsx
@@ -0,0 +1,66 @@
+
+//import './App.css'
+
+import { usePlaidLink } from 'react-plaid-link';
+import axios from "axios"
+import { useEffect, useState } from "react"
+import PropTypes from "prop-types"
+axios.defaults.baseURL = "http://localhost:8000"
+
+function PlaidAuth({publicToken}){
+  const [account, setAccount] = useState();
+
+  useEffect(()=>{
+    async function fetchData(){
+      let accessToken = await axios.post("/exchange_public_token", {public_token: publicToken});
+      console.log("access" + accessToken.data)
+      const auth = await axios.post("/auth", {access_token: accessToken.data.accessToken});
+      console.log("auth data", auth.data)
+      setAccount(auth.data.numbers.ach[0])
+    }
+    fetchData()
+  }, []);
+  return account && (
+    <>
+  <p>Account number: {account.account}</p>
+  <p>Routing number: {account.routing}</p>
+  </>
+  );
+}
+
+PlaidAuth.propTypes = {
+  publicToken: PropTypes.string,  // Ensures publicToken is a string and is required
+};
+
+
+function App() {
+  const [linkToken, setLinkToken] = useState();
+  const [publicToken, setPublicToken] = useState();
+
+  useEffect(()=>{
+    async function fetch(){
+      const response = await axios.post("/create_link_token")
+      //console.log("response",response.data)
+      setLinkToken(response.data.link_token)
+      console.log(response.data)
+    }
+    fetch()
+  },[]);
+
+  const { open, ready } = usePlaidLink({
+    token: linkToken,
+    onSuccess: (public_token, metadata) => {
+    // send public_token to server
+    setPublicToken(public_token);
+    console.log("success",public_token,metadata)
+    },
+  });
+
+  return publicToken ? (<PlaidAuth publicToken={publicToken}/>) : (
+    <button onClick={() => open()} disabled={!ready}>
+      Connect a bank account
+    </button>
+  );
+}
+
+export default App
---
+++ server/index.js
@@ -0,0 +1,100 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const { Configuration, PlaidApi, PlaidEnvironments } = require('plaid');
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const plaidClientId = process.env.PLAID_CLIENT_ID;
+const plaidSecret = process.env.PLAID_SECRET;
+const plaidEnv = process.env.PLAID_ENV;
+
+
+const configuration = new Configuration({
+  basePath: PlaidEnvironments.sandbox,
+  baseOptions: {
+    headers: {
+      'PLAID-CLIENT-ID': plaidClientId,
+      'PLAID-SECRET': plaidSecret,
+    },
+  },
+});
+
+const plaidClient = new PlaidApi(configuration);
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post("/hello", (req, res) => {
+  res.json({ message: "hello " + req.body.name });
+});
+
+app.post('/create_link_token', async (req, res) => {
+  const plaidRequest = {
+    user: {
+      client_user_id: "user",
+    },
+    client_name: 'Plaid Test App',
+    products: ['auth'],
+    language: 'en',
+    redirect_uri: 'http://localhost:5173/',
+    country_codes: ['US'],
+  };
+  try {
+    const createTokenResponse = await plaidClient.linkTokenCreate(plaidRequest);
+    res.json(createTokenResponse.data);
+  } catch (error) {
+    res.status(500).send("failure");
+  }
+});
+
+app.post("/auth", async function (request,response) {
+    try{
+        const access_token = request.body.access_token;
+        const plaidRequest = {
+            access_token: access_token,
+        };
+        const plaidResponse = await plaidClient.authGet(plaidRequest);
+        response.json(plaidResponse.data);
+         
+          
+    }catch(e){
+        response.status(500).send("failed")
+    }
+})
+
+
+
+
+app.post('/exchange_public_token', async function (
+    request,
+    response,
+    next,
+  ) {
+    const publicToken = request.body.public_token;
+    try {
+      const plaidResponse = await plaidClient.itemPublicTokenExchange({
+        public_token: publicToken,
+      });
+  
+      // These values should be saved to a persistent database and
+      // associated with the currently signed-in user
+      const accessToken = plaidResponse.data.access_token;
+      const itemID = plaidResponse.data.item_id;
+  
+      response.json({ accessToken});
+    } catch (error) {
+      // handle error
+      response.status(500).send("failed")
+    }
+  });
+
+
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server has started on port ${PORT}`);
+});
