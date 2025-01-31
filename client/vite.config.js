46e189a85b304a61fdcda7baafd911aafc418243
marenas1 <140763896+marenas1@users.noreply.github.com>
2025-01-31 18:46:47 -0500

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    port: import.meta.env.PORT ? parseInt(import.meta.env.PORT) : 5173, // Use process.env.PORT if available, otherwise fallback to 4173
-  },
   plugins: [react()],
 })
