5980ac61b15737c02c0bec571db907eb1adeeed5
marenas1 <140763896+marenas1@users.noreply.github.com>
2025-02-03 19:51:50 -0500

    render basename

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'wealthwise-banking-client-production.up.railway.app/',
+  base: 'https://wealthwise-banking-client.onrender.com',
   plugins: [react()],
 })
